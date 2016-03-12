b939de09d964175a4e70b1f7c91b16d0c9b26161
maximedelpit <maximedelpit@gmail.com>
2016-03-12 21:20:49 +0100

    user signup - i18n to do

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user! #NB => TO CHANGE
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # Defines profile type at user creation => permitted params management => Devise
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password,
+     :password_confirmation, :first_name, :last_name, :phone, :gender, :age,
+     :country_of_origin, :address, :arrival_date, user_languages_attributes: [:id, :language_id, :_destroy] )
+    }
+  end
 end
