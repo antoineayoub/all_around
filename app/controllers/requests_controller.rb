5085d83fbe11f91c89d9d7c22a51801562644245
antoine ayoub <antoine.ayoub@gmail.com>
2016-03-13 14:56:50 +0100

    request pages for refugee

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,41 +1,29 @@
 class RequestsController < ApplicationController
 
   def index
-    # Conversation means request pending or solved
-    @conversations = current_user.conversations
-    if params[:request_id]
-      @selected_conversation = @conversations.find(params[:request_id])
-    else
-      @selected_conversation = @conversations.first
-    end
-    if @selected_conversation
-      @unread_messages = @selected_conversation.unread_messages(current_user)
-      @unread_messages.each { |message| message.mark_as_read }
-      @unread_conversations_count = current_user.unread_conversations_count
-    end
-
-    # We then listed not assigned
-    if current_user.category == "volunteer"
-      @not_assigned_requests = Request.where(status: "not_assigned")
-    else
-      @not_assigned_requests = current_user.requests.where(status: "not_assigned")
-    end
-  end
-
-  def new
-    @category = params[:category]
-    @request = Request.new
+   @request = Request.new
+   @requests = current_user.requests.select do |request|
+      request.persisted?
+   end
   end
 
   def create
     @request = Request.new(request_params)
     @request.refugee = current_user
     @request.status = 'not assigned'
+    raise
     if @request.save
-      redirect_to requests_path
+      respond_to do |format|
+        raise
+        format.html { redirect_to requests_path }
+        format.js
+      end
     else
-      @category = params[:request][:category]
-      render :new
+      respond_to do |format|
+        @category = params[:request][:category]
+        format.html { render requests_path }
+        format.js  # <-- idem
+      end
     end
   end
 
